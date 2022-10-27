6b9bab21cb0cc962ea0c67c71b950c284e53ff92
Chimobi Ejiofor <15183623+mobiblunt@users.noreply.github.com>
2022-10-27 12:53:32 +0000

    broke it

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -1,17 +1,120 @@
 import React from "react"
 
 export default function Quiz(props) {
-    return (
-        <div id="second">
-      <div>
-      <h3 id="questions">{props.ques}</h3>
-      <div id="option">
-        <p className="options">{props.options}</p>
+
+  const [starWarsData, setStarWarsData] = React.useState()
+  
+  const [active, setActive] = React.useState(false);
+
+
+  let QuestionsArray = []
+  let quizElements = []
+
+  const getGitHubUserWithFetch = async () => {
+  const response = await fetch(gitHubUrl)
+  const jsonData = await response.json()
+  apiToState(jsonData)
+  }
+const handleClick = () => {
+    setActive(!active);
+  };
+
+
+  function apiToState(obj) {
+    const results = obj.results;
+
+    const arrOrder = results.map((questionSet) => ({
+      id: nanoid(),
+      question: questionSet.question,
+      ansArr: getAnsArr(
+        questionSet.correct_answer,
+        questionSet.incorrect_answers
+      ),
+      correct: false,
+    }));
+    console.log('arrOrder - ' + arrOrder);
+    setStarWarsData(arrOrder);
+  }
+
+  
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+    return array;
+  }
+
+  
+
+  function getAnsArr(correct, incorrect) {
+    const arrScram = [
+      {
+        value: correct,
+        selected: false,
+        correct: true,
+        id: nanoid(),
+      },
+    ];
+
+    for (let i = 0; i < incorrect.length; i++) {
+      arrScram.push({
+        value: incorrect[i],
+        selected: false,
+        correct: false,
+        id: nanoid(),
+      });
+    }
+    return shuffle(arrScram);
+  }
+
+
+
+  React.useEffect(function() {
         
-      </div>
-      <hr></hr>
-        </div>
+        getGitHubUserWithFetch();
+        
+           
+    
+    },[])
+
+
+  
+
+  
+    return (
+        <>
+           <h3 id="questions">{obj.question}</h3>
+               <div id="option">
+                 {obj.ansArr.map((obj) => {
+               return (
+                   <p key={obj.id} onClick={() =>handleClick()} className="options" style={{ backgroundColor: active ? "black" : "white" }}>{obj.value}</p>
+                 
+                 )})}
+               
+                 </div>
+             <hr></hr>
+      )})  
+            
+          }
+
+
+
+        
       
-    </div>
+      
+      
+        </>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,25 @@
 import React from "react"
 import './App.css'
-import Quiz from '../components/Quiz'
 import { nanoid } from 'nanoid'
+import Quiz from "../components/Quiz"
 
 
 const gitHubUrl = "https://opentdb.com/api.php?amount=5&type=multiple";
+
   
 export default function App() {
-  const [starWarsData, setStarWarsData] = React.useState()
+
+  const [intro, setIntro] = React.useState(true);
   
-
-
-  let QuestionsArray = []
-  let quizElements = []
-
-  const getGitHubUserWithFetch = async () => {
-  const response = await fetch(gitHubUrl)
-  const jsonData = await response.json()
-  apiToState(jsonData)
-  }
-
-  function apiToState(obj) {
-    const results = obj.results;
-
-    const arrOrder = results.map((questionSet) => ({
-      id: nanoid(),
-      question: questionSet.question,
-      ansArr: getAnsArr(
-        questionSet.correct_answer,
-        questionSet.incorrect_answers
-      ),
-      correct: false,
-    }));
-    console.log('arrOrder - ' + arrOrder);
-    setStarWarsData(arrOrder);
+  function handleIntro() {
+    setIntro((old) => !old);
   }
 
   
-  function shuffle(array) {
-    let currentIndex = array.length,
-      randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex !== 0) {
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex],
-        array[currentIndex],
-      ];
-    }
-    return array;
-  }
-
-  
-
-  function getQuizzes() {
-    if(QuestionsArray) {
-      quizElements = QuestionsArray.map(quiz => <Quiz ques={quiz.question} />)
-       console.log(quizElements)
-      
-    }
-    
-  }
-
-  function getAnsArr(correct, incorrect) {
-    const arrScram = [
-      {
-        value: correct,
-        selected: false,
-        correct: true,
-        id: nanoid(),
-      },
-    ];
-
-    for (let i = 0; i < incorrect.length; i++) {
-      arrScram.push({
-        value: incorrect[i],
-        selected: false,
-        correct: false,
-        id: nanoid(),
-      });
-    }
-    return shuffle(arrScram);
-  }
-
   
   
     
-    React.useEffect(function() {
-        
-        getGitHubUserWithFetch();
-        
-           console.log(starWarsData) 
     
-    },[])
-
 
 
 
@@ -110,42 +31,14 @@ export default function App() {
     <main>
       {
         
-            starWarsData
+            !intro
             ?
-    <div id="second">
-      <div>
-
-        {
-         starWarsData.map((obj) => {
-           return (
-             <>
-           <h3 id="questions">{obj.question}</h3>
-               <div id="option">
-                 {obj.ansArr.map((obj) => {
-               return (
-                   <p className="options">{obj.value}</p>
-                 
-                 )})}
-               
-                 </div>
-             <hr></hr></>
-         )})  
-            
-          }
-
-
-
-        
-      
-      
-      
-        </div>
-      
-    </div>
+        <Quiz/>
+    
     :
       <div id="header" > <h1 >Quizically</h1>
         <p>some kind of description</p>
-      <button id="button">Start Quiz</button>
+      <button onClick={handleIntro} id="button">Start Quiz</button>
         
       </div>
      }
